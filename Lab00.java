641ba4586c88d32c20977b0126f07240a3d04eca
laviy <yonatan_lavi@asl.org>
2014-10-22 10:47:03 +0100

    answered questions

--- Lab00.java
+++ Lab00.java
@@ -17,6 +17,15 @@ public class Lab00 {
    Display.setSpeed(3);
    Robot karel = new Robot();
    karel.move();
+   karel.pickBeeper();
    karel.move();
+   karel.turnLeft();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.turnLeft();
+   karel.turnLeft();
+   
+ 
      }
 }
